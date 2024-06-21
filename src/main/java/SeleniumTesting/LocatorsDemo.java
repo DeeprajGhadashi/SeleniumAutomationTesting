0020e19282dfaeb6e21137aea524fea62a785f8d
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-21 11:12:28 +0530

    name locator is used to locate

---
+++ src/main/java/SeleniumTesting/LocatorsDemo.java
@@ -0,0 +1,20 @@
+package SeleniumTesting;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LocatorsDemo {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+	    driver.get("https://demo.opencart.com/");
+	    driver.manage().window().maximize();
+	    
+	    //name
+	    driver.findElement(By.name("search")).sendKeys("Macbook");
+
+	}
+
+}
