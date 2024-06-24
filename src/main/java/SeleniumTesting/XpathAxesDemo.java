d53310847b4cea6686da7716a759bf197a5c5857
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-24 13:02:54 +0530

    Locators - XPath Axes | Self - Selects the current node

---
+++ src/main/java/SeleniumTesting/XpathAxesDemo.java
@@ -0,0 +1,22 @@
+package SeleniumTesting;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
+
+public class XpathAxesDemo {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+	    driver.get("https://money.rediff.com/gainers/bse/daily/groupa");
+	    driver.manage().window().maximize();
+	    
+	    //Self - Selects the current node
+	    String text = driver.findElement(By.xpath("//a[contains(text(),'L&T')]/self::a")).getText();
+	    System.out.println("Self : " + text);  // L&T
+	    
+	}
+
+}
