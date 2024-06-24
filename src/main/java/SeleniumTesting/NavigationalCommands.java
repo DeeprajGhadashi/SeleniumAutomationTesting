9223993063ad15c0e683b36560da4a82b61d4b94
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-25 00:47:17 +0530

    .navigate().back() | .navigate().forward()

--- src/main/java/SeleniumTesting/NavigationalCommands.java
+++ src/main/java/SeleniumTesting/NavigationalCommands.java
@@ -16,12 +16,24 @@ public class NavigationalCommands {
 		//driver.get("https://demo.nopcommerce.com/"); //accepts URL only in the string format
 		
 		 //accept URL in the String format and URL object format
-		
+		/*
 		driver.navigate().to("https://demo.nopcommerce.com/");  //String format
 
+              //OR 
 		URL myurl = new URL ("https://demo.nopcommerce.com/");   //URL object format
 		driver.navigate().to(myurl);	    
-
+       */
+		
+		driver.navigate().to("https://demo.nopcommerce.com/");
+		driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		//navigate().back()
+		driver.navigate().back();
+		System.out.println(driver.getCurrentUrl()); 
+		
+		//navigate().forward()
+		driver.navigate().forward();
+		System.out.println(driver.getCurrentUrl()); 
 	}
 
 }
