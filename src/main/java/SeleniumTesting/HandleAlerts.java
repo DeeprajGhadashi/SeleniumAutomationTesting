0b0a91c92cce941f68b500ce21c0d66c455e4e22
DeeprajGhadashi <deeprajghadshi1920@gmail.com>
2024-06-25 12:28:51 +0530

    HandleAlerts | Normal alert with OK button

---
+++ src/main/java/SeleniumTesting/HandleAlerts.java
@@ -0,0 +1,32 @@
+package SeleniumTesting;
+
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HandleAlerts {
+
+	public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+        driver.manage().window().maximize();
+        
+        //1) Normal alert with OK button
+        
+        driver.findElement(By.xpath("//button[normalize-space()='Click for JS Alert']")).click();
+        Thread.sleep(5000);
+        
+        Alert myalert = driver.switchTo().alert();
+        System.out.println(myalert.getText());
+        myalert.accept();
+        
+     
+
+	}
+
+}
